0a56b96dfa6c498fbd8eaa94d1ce016dce258d02
sureshsmart123 <sureshkumar.pacet@gmail.com>
2022-02-15 14:27:40 +0530

    Update Testnew.java

--- Testnew.java
+++ Testnew.java
@@ -5,5 +5,6 @@ public static void main(String args[])
 {
 System.out.println("Welcome to the Java");
   System.out.println("Welcome to Test automate");
+    System.out.println("Welcome to build");
 }
 }
